ea18b5f924194fe06009c2b575458ca93f7d86da
dead inside <90174775+xxxvvvxxxvx@users.noreply.github.com>
2022-03-29 06:18:03 +0000

    1

--- main.py
+++ main.py
@@ -1,49 +1,68 @@
-import sqlite3 
+import sqlite3
 
-db = sqlite3.connect('test.db')
+db=sqlite3.connect('test.db')
 
-db.execute("""
-           CREATE TABLE IF NOT EXISTS darbinieki
-           (id INTEGER PRIMARY KEY  NOT NULL, 
-           vards TEXT   NOT NULL,
-           alga REAL,
-           piezimes CHAR(50)
+
+
+db.execute(""" CREATE TABLE IF NOT EXISTS darbinieki
+           (id    INTEGER  PRIMARY KEY  NOT NULL,
+           vards   TEXT  NOT NULL,
+           alga    REAL,
+           piezimes  CHAR(50)
            )
            """)
 
-db.execute("""INSERT INTO darbinieki
+db.execute(""" INSERT INTO darbinieki
            (id,vards,alga,piezimes)
-           VALUES(1, 'Anna', 900.50, 'ljaljaljalja')
+           VALUES(1,"Anna",980.50,"Ekonomiskas")
            """)
 
 db.commit()
 
-dati = db.execute("""SELECT * FROM darbinieki
-                  WHERE id=1
-                  """)
 
-rezultats = dati.fetchall()
+dati=db.execute("""SELECT*FROM darbinieki
+                WHERE id=]
+               """)
+rezultats=dati.fetcha
 print(rezultats)
 
 db.execute(
-  """
-  CREATE TABLE IF NOT EXISTS
-  (id INTEGER PRIMARY KEY AUTOINCREMENT
-  pceturksnis REAL,
-  oceturksnis REAL,
-  tceturksnis REAL,
-  cceturksnis REAL,
-  )
-  
-  """
+"""
+CREATE TABLE IF NOT EXISTING
+(id  INTEGER  PRIMARY KEY  AUTOINCREMENT  NOT NULL,
+pceturksnis REAL,
+oceturksnis REAL,
+tceturksnis REAL,
+cceturksnis REAL
 )
+"""
+)
+p_ceturksnis=Input('Ievadi pirmā ceturksņa prēmiju')
+o_ceturksnis=Input('Ievadi otrā ceturksņa prēmiju')
+t_ceturksnis=Input('Ievadi trešā ceturksņa prēmiju')
+c_ceturksnis=Input('Ievadi ceturtā ceturksņa prēmiju')
+db.execute(
+"""
+INSERT INTO premijas
+(pceturksnis, oceturksnis, tceturksnis, cceturksnis)
+VALUES[:'pceturksnis,':'p_ceturksnis','oceturksnis':o_ceturksnis,'tceturksnis':t_ceturknis,'cceturksnis':c_ceturksnis ]
+"""
+)
+db.commit()
+dati =db.execute("""SELECT*FROM premijas
+                 WHERE id=1
+                 """)
 
-print("tt:")
+dati = db.execute(""" SELECT * FROM darbinieki
+                  JOIN premijas
+                  ON darbinieki.id = premijas.id
+                  """)
 
-db.execute("""
-           INSERT INTO premijas:
-           ()
-           VALUES(:)
-          
-           """)
+rezultats =dati.fetchall()
+print(rezultats)
 
+
+for rinda in rezultats:
+  print("ID:", rinda[0])
+  print("Vārds:"rinda[1])
+  print("Alga:"rinda[2])
